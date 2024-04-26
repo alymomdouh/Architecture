7564d264bdde10f28a4b56d69ec3740ece259bc9
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-04-27 01:54:03 +0300

    add cqrs command and query and response and model for student

---
+++ school-clean-architecture/School.Core/Features/Students/Queries/Models/GetStudentByIDQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using School.Core.Bases;
+using School.Core.Features.Students.Queries.Results;
+
+namespace School.Core.Features.Students.Queries.Models
+{
+    public class GetStudentByIDQuery : IRequest<Response<GetSingleStudentResponse>>
+    {
+        public int Id { get; set; }
+        public GetStudentByIDQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ school-clean-architecture/School.Core/Features/Students/Queries/Models/GetStudentPaginatedListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using School.Common.Enums;
+using School.Core.Features.Students.Queries.Results;
+using School.Core.Wrappers;
+
+namespace School.Core.Features.Students.Queries.Models
+{
+    public class GetStudentPaginatedListQuery : IRequest<PaginatedResult<GetStudentPaginatedListResponse>>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public StudentOrderingEnum OrderBy { get; set; }
+        public string? Search { get; set; }
+    }
+}
