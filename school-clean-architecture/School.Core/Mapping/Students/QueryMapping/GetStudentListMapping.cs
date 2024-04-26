62caef6f47ca3bb2ddb84d070e9c1a9a6c40cad2
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-04-27 01:54:27 +0300

    add mapping for student

---
+++ school-clean-architecture/School.Core/Mapping/Students/QueryMapping/GetStudentListMapping.cs
@@ -0,0 +1,15 @@
+﻿using School.Core.Features.Students.Queries.Results;
+using School.Data.Entities;
+
+namespace School.Core.Mapping.Students
+{
+    public partial class StudentProfile
+    {
+        public void GetStudentListMapping()
+        {
+            CreateMap<Student, GetStudentListResponse>()
+               .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department.Localize(src.Department.DNameAr, src.Department.DNameEn)))
+               .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Localize(src.NameAr, src.NameEn)));
+        }
+    }
+}
