3b3b2ead86ae7b7595c369fdcd8c92f27693d9b7
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-04-27 02:07:54 +0300

    add ErrorHandlerMiddleware and register in program and other

--- school-clean-architecture/School.Api/Program.cs
+++ school-clean-architecture/School.Api/Program.cs
@@ -1,9 +1,14 @@
 
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using School.Core;
+using School.Core.MiddleWare;
 using School.Infrustructure;
 using School.Infrustructure.Context;
 using School.Service;
-using School.Core;
+using Serilog;
+using System.Globalization;
 
 namespace School.Api
 {
@@ -35,6 +40,49 @@ namespace School.Api
                             .AddServiceRegisteration(builder.Configuration);
             #endregion
 
+            #region Localization
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddLocalization(opt =>
+            {
+                opt.ResourcesPath = "";
+            });
+
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                List<CultureInfo> supportedCultures = new List<CultureInfo>
+    {
+            new CultureInfo("en-US"),
+            new CultureInfo("de-DE"),
+            new CultureInfo("fr-FR"),
+            new CultureInfo("ar-EG")
+    };
+                options.DefaultRequestCulture = new RequestCulture("en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+            #endregion
+
+            #region AllowCORS
+            var CORS = "_cors";
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: CORS,
+                                  policy =>
+                                  {
+                                      policy.AllowAnyHeader();
+                                      policy.AllowAnyMethod();
+                                      policy.AllowAnyOrigin();
+                                  });
+            });
+
+            #endregion
+
+            #region Serilog
+            Log.Logger = new LoggerConfiguration()
+                          .ReadFrom.Configuration(builder.Configuration).CreateLogger();
+            builder.Services.AddSerilog();
+            #endregion
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -44,10 +92,19 @@ namespace School.Api
                 app.UseSwaggerUI();
             }
 
+            #region Localization Middleware
+            var options = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(options.Value);
+            #endregion
+
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+
             app.UseHttpsRedirection();
+            app.UseCors(CORS);
+            app.UseStaticFiles();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
