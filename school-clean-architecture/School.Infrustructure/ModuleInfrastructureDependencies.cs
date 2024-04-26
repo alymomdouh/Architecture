aa76ac6f34160eeea22624daad3a6e5bb96d13cc
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-04-27 02:00:46 +0300

    register services and Repository and AddMediatR

---
+++ school-clean-architecture/School.Infrustructure/ModuleInfrastructureDependencies.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using School.Infrustructure.Abstracts;
+using School.Infrustructure.InfrastructureBases;
+using School.Infrustructure.Repositories;
+
+namespace School.Infrustructure
+{
+    public static class ModuleInfrastructureDependencies
+    {
+        public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<IStudentRepository, StudentRepository>();
+            services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+            services.AddTransient<IInstructorsRepository, InstructorsRepository>();
+            services.AddTransient<ISubjectRepository, SubjectRepository>();
+            //services.AddTransient<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddTransient(typeof(IGenericRepositoryAsync<>), typeof(GenericRepositoryAsync<>));
+
+            ////views
+            //services.AddTransient<IViewRepository<ViewDepartment>, ViewDepartmentRepository>();
+
+            ////Procedure
+            //services.AddTransient<IDepartmentStudentCountProcRepository, DepartmentStudentCountProcRepository>();
+
+            ////functions
+            //services.AddTransient<IInstructorFunctionsRepository, InstructorFunctionsRepository>();
+
+            return services;
+        }
+    }
+}
