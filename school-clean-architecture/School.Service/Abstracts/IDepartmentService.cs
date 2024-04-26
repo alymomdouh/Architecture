903cbb507816d1eb1a2cca0d310d3cd6676800ed
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-04-27 01:57:11 +0300

    add services for main tables

---
+++ school-clean-architecture/School.Service/Abstracts/IDepartmentService.cs
@@ -0,0 +1,12 @@
+﻿using School.Data.Entities;
+
+namespace School.Service.Abstracts
+{
+    public interface IDepartmentService
+    {
+        public Task<Department> GetDepartmentById(int id);
+        public Task<bool> IsDepartmentIdExist(int departmentId);
+       // public Task<List<ViewDepartment>> GetViewDepartmentDataAsync();
+       // public Task<IReadOnlyList<DepartmentStudentCountProc>> GetDepartmentStudentCountProcs(DepartmentStudentCountProcParameters parameters);
+    }
+}
