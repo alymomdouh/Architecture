01d1d2e94f1df0d2d161651237b9e0e052d11629
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-04-27 12:49:25 +0300

    convert record to class

--- school-clean-architecture/School.Core/Features/Students/Queries/Results/GetStudentListResponse.cs
+++ school-clean-architecture/School.Core/Features/Students/Queries/Results/GetStudentListResponse.cs
@@ -1,13 +1,13 @@
 ﻿namespace School.Core.Features.Students.Queries.Results
 {
-    //public class GetStudentListResponse
-    //{
-    //    public int StudID { get; set; }
-    //    public string? Name { get; set; }
-    //    public string? Address { get; set; }
-    //    public string? DepartmentName { get; set; }
-    //}
-    public record GetStudentListResponse(int StudID, string? Name, string? Address, string? DepartmentName)
-    { 
+    public class GetStudentListResponse
+    {
+        public int StudID { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? DepartmentName { get; set; }
     }
+    //public record GetStudentListResponse(int StudID, string? Name, string? Address, string? DepartmentName)
+    //{ 
+    //}
 }
--- school-clean-architecture/School.Core/Features/Students/Queries/Results/GetStudentPaginatedListResponse.cs
+++ school-clean-architecture/School.Core/Features/Students/Queries/Results/GetStudentPaginatedListResponse.cs
@@ -1,13 +1,13 @@
 ﻿namespace School.Core.Features.Students.Queries.Results
 {
-    //public class GetStudentPaginatedListResponse
-    //{
-    //    public int StudID { get; set; }
-    //    public string? Name { get; set; }
-    //    public string? Address { get; set; }
-    //    public string? DepartmentName { get; set; }
-    //}
-    public record GetStudentPaginatedListResponse(int StudID, string? Name, string? Address, string? DepartmentName)
+    public class GetStudentPaginatedListResponse
     {
+        public int StudID { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? DepartmentName { get; set; }
     }
+    //public record GetStudentPaginatedListResponse(int StudID, string? Name, string? Address, string? DepartmentName)
+    //{
+    //}
 }
